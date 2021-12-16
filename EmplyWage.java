09c386cef7e882348f2ff831b4866a64d0e53857
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-16 18:01:36 +0530

    UC9Emplywage

--- EmplyWage.java
+++ EmplyWage.java
@@ -1,9 +1,16 @@
 package Emplywage;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//import AddressBook.Person;
+
+//import AddressBook.Person;
 
 public class EmplyWage {
-
+//	static ArrayList<Person> arr = new ArrayList<>();
+	static Scanner sc = new Scanner(System.in);
+	static int rate1, days1, count, Present_wage1, Part_Time_wage1, Absent_wage1;
 	static int present, absent, Part_Time_days1;// (UC1 to UC3)
 	static int Prasent1, absent1, Partime1; // (UC4 to UC7)
 
@@ -11,7 +18,7 @@ public class EmplyWage {
 	final static int Part_Time_Fixed_hrs = 4; // can not be changed
 
 	static int days, Rate;// (UC1 to UC3)
-	static String name;// (UC1 to UC3)
+	static String name, name1;// (UC1 to UC3)
 
 	/*
 	 * UC1 EMPLOYEE WAGE
@@ -103,8 +110,6 @@ public class EmplyWage {
 	 * UC4 EMPLOYEE WAGE
 	 */
 	public static void EmplyWageSwitchCase() {
-		String name1;
-		int rate1, days;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the employee name :");
@@ -137,14 +142,13 @@ public class EmplyWage {
 			} // end of while
 
 		} // end of for loop
-
 		/*
 		 * calculating the Wages for:- [equation=(rate*Hours*days)] Present days Absent
 		 * days
 		 */
-		int Present_wage1 = rate1 * Fixed_hrs * Prasent1;// Present days
-		int Part_Time_wage1 = rate1 * Part_Time_Fixed_hrs * Partime1; // Part_Time days
-		int Absent_wage1 = rate1 * Fixed_hrs * absent1; // Absent days
+		Present_wage1 = rate1 * Fixed_hrs * Prasent1;// Present days
+		Part_Time_wage1 = rate1 * Part_Time_Fixed_hrs * Partime1; // Part_Time days
+		Absent_wage1 = rate1 * Fixed_hrs * absent1; // Absent days
 		/*
 		 * Displaying the Total days
 		 */
@@ -173,61 +177,83 @@ public class EmplyWage {
 	}
 
 	/*
+	 * main code for re-using the funtion
+	 */
+	public static void maincodeEmplyWage() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\n\nEnter the company name :");
+		name1 = sc.next();
+		System.out.print("Enter number of working days :");
+		days = sc.nextInt();
+		System.out.print("Enter employee wage rate :");
+		rate1 = sc.nextInt();
+		System.out.println("\nThe company name is : " + name1);
+
+		for (int i = 1; i <= days; i++) {
+			int emp = (int) Math.floor(Math.random() * 10 % 3);
+			switch (emp) {
+			case 0:
+				absent1++;
+				break;
+			case 1:
+				Prasent1++;
+				break;
+			case 2:
+				Partime1++;
+				break;
+
+			} // end of while
+
+		} // end of for loop
+
+		/*
+		 * calculating the Wages for:- [equation=(rate*Hours*days)] Present days Absent
+		 * days
+		 */
+		Present_wage1 = rate1 * Fixed_hrs * Prasent1;// Present days
+		Part_Time_wage1 = rate1 * Part_Time_Fixed_hrs * Partime1; // Part_Time days
+		Absent_wage1 = rate1 * Fixed_hrs * absent1; // Absent days
+
+	}
+
+	/*
 	 * UC8 EMPLOYEE WAGE
 	 */
 	public static void EmplyWageCompany() {
-		String name1;
-		int rate1, days, count;
-
 		Scanner sc = new Scanner(System.in);
+		EmplyWage e = new EmplyWage();
 		System.out.print("How many companies you want Compute Employee Wage :");
 		count = sc.nextInt();
 
 		for (int j = 1; j <= count; j++) {
-			System.out.print("\n**********************************************************");
-			System.out.print("\n\nEnter the company name :");
-			name1 = sc.next();
-			System.out.print("Enter number of working days :");
-			days = sc.nextInt();
-			System.out.print("Enter employee wage rate :");
-			rate1 = sc.nextInt();
-			System.out.println("\nThe company name is : " + name1);
-
-			for (int i = 1; i <= days; i++) {
-				int emp = (int) Math.floor(Math.random() * 10 % 3);
-				switch (emp) {
-				case 0:
-					absent1++;
-					break;
-				case 1:
-					Prasent1++;
-					break;
-				case 2:
-					Partime1++;
-					break;
-
-				} // end of while
-
-			} // end of for loop
-
-			/*
-			 * calculating the Wages for:- [equation=(rate*Hours*days)] Present days Absent
-			 * days
-			 */
-			int Present_wage1 = rate1 * Fixed_hrs * Prasent1;// Present days
-			int Part_Time_wage1 = rate1 * Part_Time_Fixed_hrs * Partime1; // Part_Time days
-			int Absent_wage1 = rate1 * Fixed_hrs * absent1; // Absent days
+//			System.out.print("\n**********************************************************");
+			maincodeEmplyWage();
 			/*
 			 * Displaying the Total days
 			 */
 //		System.out.println("\nStopping the program at 100hrs : " + total_Working_Hrs);
 			System.out.println("\nThe present days is " + Prasent1 + " and wage amount is : ₹ " + Present_wage1);
-			System.out.println("The absent days is " + absent1 + "  wage amount is : ₹ -"+Absent_wage1);
+			System.out.println("The absent days is " + absent1 + "  wage amount is : ₹ -" + Absent_wage1);
 			System.out.println("The part time days is " + Partime1 + " wage wage amount is : ₹ " + Part_Time_wage1);
 
+			Prasent1 = 0;
+			absent1 = 0;
+			Partime1 = 0;
 		} // END OF 1st FOR LOOP
+
 	}
 
+	/*
+	 * UC9 EMPLOYEE WAGE
+	 */
+	public static void EmplyCompanywage() {
+		EmplyWageCompany();
+	}
+
+	/*
+	 * inner class start
+	 */
+	
 	/*
 	 * Main Function
 	 */
@@ -243,7 +269,9 @@ public class EmplyWage {
 //		e.EmplyWageSwitchCase();	 // UC4
 //		e.EmplyWage20Days(); 		 // UC5
 //		e.EmplyWage100HrsAnd20Days();// UC6
-		e.EmplyWageCompany(); // UC8
+//		e.EmplyWageCompany(); // UC8
+		e.EmplyCompanywage(); //UC9
+
 
 	}
 }
